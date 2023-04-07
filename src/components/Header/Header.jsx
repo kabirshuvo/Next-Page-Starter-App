beb99688f5aa622cc23e1626bbe522394301808c
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-08 04:51:17 +0600

    Nav Bar Updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { BugAntIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <nav className='flex items-center justify-between'>
-            <div><h1 className='text-3xl font-bold'>The Next Page</h1></div>
-            <div className='mr-7 flex justify-evenly gap-7'>
-               <Link to="/">Home</Link>
-               <Link to="/about">About</Link>
-               <Link to="/books">Books</Link>
+        <nav className='flex items-center justify-around'>
+            <div className='flex px-4 gap-7 items-center'>
+                
+            <BugAntIcon className="h-6 w-6 text-purple-700" />
+                <h1 className='text-3xl font-bold text-purple-500 tracking-widest'>The Next Page</h1></div>
+            <div className='mr-7 flex justify-evenly gap-7 font-bold'>
+                {/* className={({isActive}) => isActive ? 'text-purple-500' : '' } */}
+               <NavLink to="/" >Home</NavLink>
+               <NavLink to="/about">About</NavLink>
+               <NavLink to="/books"  >Books</NavLink>
             </div>
         </nav>
     );
