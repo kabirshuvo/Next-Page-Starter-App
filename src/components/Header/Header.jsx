568517830ef595d528a17f00c0761c64533432a8
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-08 22:48:27 +0600

    Menu Bar Icon Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,43 @@
-import { BugAntIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { Bars3BottomRightIcon, BugAntIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+      };
     return (
-        <nav className='flex items-center justify-around'>
-            <div className='flex px-4 gap-7 items-center'>
+<nav className="bg-purple-700 py-4">
+<div className="container mx-auto flex items-center justify-between">
                 
-            <BugAntIcon className="h-6 w-6 text-purple-700" />
-                <h1 className='text-3xl font-bold text-purple-500 tracking-widest'>The Next Page</h1></div>
-            <div className='mr-7 flex justify-evenly gap-7 font-bold'>
+    <BugAntIcon className="h-6 w-6 text-white" />
+    <h1 className="text-lg font-bold ml-2 text-white tracking-widest">The Next Page</h1>
+
+    <div className="md:flex hidden">
                 {/* className={({isActive}) => isActive ? 'text-purple-500' : '' } */}
-               <NavLink to="/" >Home</NavLink>
-               <NavLink to="/about">About</NavLink>
-               <NavLink to="/books"  >Books</NavLink>
-            </div>
-        </nav>
+               <NavLink to="/" className="text-white font-bold mx-4 hover:text-gray-300"  >Home</NavLink>
+               <NavLink to="/about" className="text-white font-bold mx-4 hover:text-gray-300" >About</NavLink>
+               <NavLink to="/books" className="text-white font-bold mx-4 hover:text-purple-300" >Books</NavLink>
+    </div>
+
+    <div className="md:hidden flex items-center">
+        <button onClick={toggleMenu} className="outline-none mobile-menu-button">
+            {isMenuOpen ? (
+              <XMarkIcon className="h-6 w-6 text-white" />
+            ) : (
+              <Bars3BottomRightIcon className="h-6 w-6 text-white" />
+            )}
+        </button>
+    </div>
+</div> 
+
+    <div className={`${isMenuOpen ? '' : 'hidden'} mobile-menu md:hidden`}>
+        <NavLink to="/" className="block text-center py-2 px-4 text-white font-bold hover:bg-gray-400" >Home</NavLink>
+        <NavLink to="/about" className="block text-center py-2 px-4 text-white font-bold hover:bg-gray-400" >About</NavLink>
+        <NavLink to="/books" className="block text-center py-2 px-4 text-white font-bold hover:bg-gray-400" >Books</NavLink>
+    </div>
+
+</nav>
     );
 };
 
